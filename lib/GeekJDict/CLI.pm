kroki/GeekJDict

commit c41b0a6cabc74c4f3ce614225c06d03fe6bf2bd7

Fix regex so that "-" and "^" are part of the glob only when inside "[..]".
This fixes the search for "self-defense", etc.

lib/GeekJDict/Util.pm
@@ -62,7 +62,8 @@ sub get_terms {
 
 
 sub get_globs {
-    return _normalize($_[0]) =~ /((?:[^\W_]|[⻌○×◎〃※〒△→←↑↓]|[]^[?*-])+)/g;
+    return _normalize($_[0]) =~
+        /((?:[^\W_]|[⻌○×◎〃※〒△→←↑↓]|\*|\?|\[(?>\^?).+?\])+)/g;
 }
 
 

commit 460984a8bdf4d2e89e847f109e5ef21f5689a8ff

Fix "-" in tag names broken by previous commit (c41b0a6).

lib/GeekJDict/CLI.pm
@@ -516,7 +516,7 @@ sub show_tags {
     my $self = shift;
     my ($input) = @_;
 
-    my @globs = get_globs($input);
+    my @globs = get_globs($input, 1);
     if (@globs) {
         shift @globs if $globs[0] eq "";
     } else {
@@ -977,7 +977,7 @@ sub find_words {
         my @condition;
         foreach my $t (@tags) {
             $t =~ s/\*\*+/*/g;
-            my @g = get_globs($t);
+            my @g = get_globs($t, 1);
             @g = grep { !/^\*$/ && !/^\?\*$/ && !/^\*\?$/ && !/^\*\?\*$/ } @g;
             if (@g) {
                 my @c;

lib/GeekJDict/Util.pm
@@ -62,8 +62,13 @@ sub get_terms {
 
 
 sub get_globs {
-    return _normalize($_[0]) =~
-        /((?:[^\W_]|[⻌○×◎〃※〒△→←↑↓]|\*|\?|\[(?>\^?).+?\])+)/g;
+    unless ($_[1]) {
+        return _normalize($_[0]) =~
+            /((?:[^\W_]|[⻌○×◎〃※〒△→←↑↓]|\*|\?|\[(?>\^?).+?\])+)/g;
+    } else {
+        return _normalize($_[0]) =~
+            /((?:[^\W_]|[⻌○×◎〃※〒△→←↑↓]|\*|\?|\[(?>\^?).+?\]|-)+)/g;
+    }
 }
 
 